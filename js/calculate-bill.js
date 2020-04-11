e345ac22fdd63342dd670289edc51bfdde0899dd
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-04-11 18:24:13 +0200

    commit changes

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,8 +1,12 @@
 //get a reference to the calculate button
-
+const calculateBtn = document.querySelector(".calculateBtn");
 //get a reference to the billTotal element
+const billTotalElement = document.querySelector(".billTotal")
+    //get a reference to the billString
+const billStringField = document.querySelector(".billString")
+    //get a reference to the total span element
+const billTotalSpanElement = document.querySelector(".total")
 
-//get a reference to the billString
 
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
@@ -11,3 +15,40 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+function calculateBtnClicked() {
+    // get the string entered in the textArea
+    var billString = billStringField.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i = 0; i < billItems.length; i++) {
+        var billItem = billItems[i].trim();
+        if (billItem === "call") {
+            billTotal += 2.75;
+        } else if (billItem === "sms") {
+            billTotal += 0.75;
+        }
+    }
+
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+    billTotalSpanElement.classList.remove("warning")
+    billTotalSpanElement.classList.remove("danger")
+
+
+
+
+
+    const currentTotal = Number(roundedBillTotal)
+    if (currentTotal >= 30) {
+        billTotalSpanElement.classList.add("danger")
+    } else if (currentTotal > 20 && currentTotal < 30) {
+        billTotalSpanElement.classList.add("warning")
+    }
+}
+
+calculateBtn.addEventListener("click", calculateBtnClicked)
