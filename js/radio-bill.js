3877ace5c1d97fa17f9d78c9fde77c68684f8403
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2020-05-28 12:30:57 +0200

    linked factory

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -4,25 +4,29 @@ var callTotal2 = document.querySelector(".callTotalTwo");
 var smsTotal2 = document.querySelector(".smsTotalTwo");
 var total2 = document.querySelector(".totalTwo");
 
+var radioBillFactory = textBillTotal();
+
+
 function radioBill() {
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-    var billItemType = checkedRadioBtn.value
-    if (billItemType === 'call') {
-        totalCalls += 2.75;
-    } else if (billItemType === 'sms') {
-        totalSms += 0.75;
-    }
-    callTotal2.innerHTML = totalCalls.toFixed(2);
-    smsTotal2.innerHTML = totalSms.toFixed(2);
-    var costTotal = totalCalls + totalSms;
-    total2.innerHTML = costTotal.toFixed(2)
-    if (costTotal >= 50) {
-        total2.classList.add("danger");
-    } else if (costTotal >= 30) {
-        total2.classList.add("warning");
-    }
-}
-var totalCalls = 0;
-var totalSms = 0;
+    if (checkedRadioBtn) {
+
+        var billItemType = checkedRadioBtn.value;
+        radioBillFactory.textBillAdd(billItemType);
+
+        var radioTotal = radioBillFactory.textBillEntered();
+        var radioColor = radioBillFactory.color(radioTotal.totalOfTextBill);
+
+        callTotal2.innerHTML = radioTotal.callOfTextBill.toFixed(2);
+        smsTotal2.innerHTML = radioTotal.smsOfTextBill.toFixed(2);
+        total2.innerHTML = radioTotal.totalOfTextBill.toFixed(2);
+
+        total2.classList.remove("warning");
+        total2.classList.remove("danger");
+        if (radioColor != "") {
+            total2.classList.add(radioColor)
+        };
+    };
+};
 
 radioBillBtn.addEventListener('click', radioBill);
