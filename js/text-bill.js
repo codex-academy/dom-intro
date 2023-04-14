3f93b7c70c32f4d9609b0d6db21300e1e70eb107
Akhona Mjikelo <akhona0304mjikelo@gmail.com>
2023-04-14 12:04:35 +0200

    second commit

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,15 +1,35 @@
-var callsTotal = 0;
-var smsTotal = 0;
+
+const callTotalOne = document.querySelector(".callTotalOne")
+const smsTotalOne = document.querySelector(".smsTotalOne")
+const totalOne = document.querySelector(".totalOne")
+
 
 // get a reference to the textbox where the bill type is to be entered
-
+const billTypeText = document.querySelector(".billTypeText")
 //get a reference to the add button
-
+const addToBillBtn = document.querySelector(".addToBillBtn")
 //create a variable that will keep track of the total bill
-
+var callsTotal = 0;
+var smsTotal = 0;
 //add an event listener for when the add button is pressed
-
+//addToBillBtn.addEventListener('click', addToBillBtn);
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+function textBillTotal(){
+    var billTypeEntered = billTypeText.value.trim();
+    if (billTypeEntered === "bill"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
 // * add the appropriate value to the running total
+callTotalOne.innerHTML = callsTotal.toFixed(2);
+smsTotalOne.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalOne.innerHTML = totalCost.toFixed(2);
+}
+
+addToBillBtn.addEventListener('click', textBillTotal);
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
